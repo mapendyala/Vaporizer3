94ad8e76cd35dee81ebdbc2be8e4b78a64c93b5c
subhchakraborty <subhchakraborty@USMUMSUBHCHAKR1.us.deloitte.com>
2015-05-15 00:28:27 +0530

    v

--- src/main/java/com/force/example/fulfillment/DataLoaderController.java
+++ src/main/java/com/force/example/fulfillment/DataLoaderController.java
@@ -117,7 +117,7 @@ public class DataLoaderController {
     	}
     	catch(Exception ex){
     		System.out.println("ERRor"+ex.getMessage());
-    		return "Error:_"+ex.getMessage();
+    		return "Error:_"+"Error Occored during Dataload";
     	}
     	//example.runSample("Account", "subhchakraborty@deloitte.com.vaporizer", "May@2013", "DataFile.csv","C:\\Users\\subhchakraborty\\Downloads\\Sep.sdl");
         
