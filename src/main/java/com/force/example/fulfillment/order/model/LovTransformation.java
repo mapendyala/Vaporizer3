d9f97500f313fa3943afa3710684d6cab7461d0d
Subrat kumar sen <skumarsen@deloitte.com>
2016-03-09 11:07:16 +0530

    commit changes for Lov

---
+++ src/main/java/com/force/example/fulfillment/order/model/LovTransformation.java
@@ -0,0 +1,27 @@
+package com.force.example.fulfillment.order.model;
+
+public class LovTransformation {
+private String lovType;
+private String sourceVal;
+private String targetVal;
+public String getLovType() {
+	return lovType;
+}
+public void setLovType(String lovType) {
+	this.lovType = lovType;
+}
+public String getSourceVal() {
+	return sourceVal;
+}
+public void setSourceVal(String sourceVal) {
+	this.sourceVal = sourceVal;
+}
+public String getTargetVal() {
+	return targetVal;
+}
+public void setTargetVal(String targetVal) {
+	this.targetVal = targetVal;
+}
+
+
+}
